21c46e9c1db863abf4c54a3adc628905bdf7e3eb
Shubham Khandekar <mrethical8@gmail.com>
2025-08-20 03:39:23 +0530

    Files Added

---
+++ Spring ORM Theory/springmvc/src/main/java/com/telusko/springmvc/HomeController.java
@@ -0,0 +1,58 @@
+package com.telusko.springmvc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.telusko.springmvc.dao.AlienDao;
+import com.telusko.springmvc.model.Alien;
+
+@Controller
+public class HomeController 
+{
+	
+	@Autowired
+	private AlienDao dao;
+	
+    @ModelAttribute	
+   public void modelData(Model m)
+   {
+	  m.addAttribute("name","Aliens"); 
+   }
+    
+   @RequestMapping("/") 
+   public String home()
+   {
+	   return "index";
+   }
+   
+  @GetMapping("getAliens")
+  public String getAliens(Model m)
+  {
+	  m.addAttribute("result", dao.getAliens());
+	  
+	  return "showAliens";
+  } 
+   
+  @GetMapping("getAlien")
+  public String getAlien(@RequestParam int aid,Model m)
+  {
+	  m.addAttribute("result", dao.getAlien(aid));
+	  
+	  return "showAliens";
+  }
+  
+   @RequestMapping("addAlien")
+   public String addAlien(@ModelAttribute("result") Alien a)
+   {   
+	   dao.addAlien(a);
+	   
+	   return "showAliens";
+   }
+   
+}
